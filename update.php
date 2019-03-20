a780beee86dec3470ef33f96a2602169a9250c32
Josh-OS <skybyte4me@gmail.com>
2019-03-20 23:41:34 +0800

    odify

--- update.php
+++ update.php
@@ -1,3 +1,14 @@
 <?php
 
 var_dump($_POST, $_GET);
+
+//WRITE DATA TO CSV
+$data = $_POST;
+$data['id'] = uniqid();
+
+var_dump($data);
+
+file_put_contents('phonebook2.csv', (implode(',', $data) . "\n"), FILE_APPEND);
+
+
+require('phonebook.php');
